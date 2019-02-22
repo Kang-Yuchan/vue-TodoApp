845296420de3eb2f06bfa102749237215d2300da
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-22 12:49:11 +0900

    start vue todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoHeader.vue'
+import TodoList from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoHeader.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
