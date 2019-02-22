7248e8a52c1c6a5ff4bd7553456d4c37687e9301
Kang.Yuchan <ikimaku0517@gmail.com>
2019-02-22 21:03:01 +0900

    lists complete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-                {{todoItem}}
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+                <i class="checkBtn fas fa-check-square" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
+                @click="toggleComplete( todoItem, index )"></i>
+                <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
                 <span class="removeBtn" @click="removeTodo(todoItem, index)">
                     <i class="fas fa-trash"></i>
                 </span> 
@@ -23,13 +25,19 @@ export default {
             console.log(todoItem, index);
             localStorage.removeItem(todoItem);
             this.todoItems.splice(index, 1);
+        },
+        toggleComplete(todoItem, index) {
+            todoItem.completed = !todoItem.completed;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
         }
     },
     created() {
         if(localStorage.length > 0) {
             for(var i = 0; i < localStorage.length; i ++) {
                if(localStorage.key(i) !== "loglevel:webpack-dev-server") {
-                   this.todoItems.push(localStorage.key(i));
+                   this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                   //this.todoItems.push(localStorage.key(i));
                }
             }
         }
@@ -37,7 +45,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 ul {
     list-style-type: none;
     padding-left: 0px;
